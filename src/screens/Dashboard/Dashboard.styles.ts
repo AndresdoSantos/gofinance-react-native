9e7d9d8f0f1f0f49747eba49209b10c6c2e456f3
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-10-27 19:23:08 -0300

    feat: finish list updates

--- src/screens/Dashboard/Dashboard.styles.ts
+++ src/screens/Dashboard/Dashboard.styles.ts
@@ -1,6 +1,13 @@
 import styled from "styled-components/native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { Feather } from "@expo/vector-icons";
+import { FlatList } from "react-native";
+import {
+  getStatusBarHeight,
+  getBottomSpace,
+} from "react-native-iphone-x-helper";
+
+import { DashboardTransactionsListProps } from ".";
 
 export const DashboardContainer = styled.View`
   flex: 1;
@@ -13,13 +20,14 @@ export const DashboardHeader = styled.View`
   background-color: ${({ theme }) => theme.colors.primary};
 
   flex-direction: row;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
 `;
 
 export const DashboardHeaderUserWrapper = styled.View`
   width: 100%;
   padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
 
   flex-direction: row;
   align-items: center;
@@ -60,3 +68,41 @@ export const DashboardHeaderLogout = styled(Feather).attrs(({ theme }) => ({
   name: "power",
   color: theme.colors.secondary,
 }))``;
+
+export const DashboardHighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingLeft: 24,
+    paddingRight: 8,
+  },
+})`
+  width: 100%;
+  position: absolute;
+
+  margin-top: ${RFPercentage(20)}px;
+`;
+
+export const DashboardTransactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+
+  margin-top: ${RFPercentage(8)}px;
+`;
+
+export const DashboardTransactionsTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => theme.colors.text_dark};
+
+  margin-bottom: 16px;
+`;
+
+export const DashboardTransactionsList = styled(
+  FlatList as new () => FlatList<DashboardTransactionsListProps>
+).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace(),
+  },
+})``;
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,3 +1,9 @@
+import { HighlightCard } from "../../components/HighlightCard";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../../components/TransactionCard";
+
 import {
   DashboardContainer,
   DashboardHeader,
@@ -8,9 +14,56 @@ import {
   DashboardHeaderUserInfo,
   DashboardHeaderUserPhoto,
   DashboardHeaderLogout,
+  DashboardHighlightCards,
+  DashboardTransactions,
+  DashboardTransactionsTitle,
+  DashboardTransactionsList,
 } from "./Dashboard.styles";
 
+export interface DashboardTransactionsListProps extends TransactionCardProps {
+  id: number;
+}
+
 export function Dashboard() {
+  const data: DashboardTransactionsListProps[] = [
+    {
+      id: 0,
+      type: "positive",
+      amount: "R$ 3.593,89",
+      category: {
+        icon: "dollar-sign",
+        key: "Vendas",
+        name: "Vendas",
+      },
+      date: "13/05/2022",
+      title: "Desenvolvimento de sites",
+    },
+    {
+      id: 1,
+      type: "negative",
+      amount: "R$ 42,56",
+      category: {
+        icon: "coffee",
+        key: "Vendas",
+        name: "Alimentação",
+      },
+      date: "13/05/2022",
+      title: "Café do dia",
+    },
+    {
+      id: 2,
+      type: "negative",
+      amount: "R$ 3.593,89",
+      category: {
+        icon: "shopping-bag",
+        key: "Vendas",
+        name: "Compras",
+      },
+      date: "13/05/2022",
+      title: "Aluguel da casa",
+    },
+  ];
+
   return (
     <DashboardContainer>
       <DashboardHeader>
@@ -29,6 +82,37 @@ export function Dashboard() {
           <DashboardHeaderLogout />
         </DashboardHeaderUserWrapper>
       </DashboardHeader>
+
+      <DashboardHighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entrada"
+          lastTransaction="Última transação em 30 de abril"
+          amount="R$ 17.450,60"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          lastTransaction="Última transação em 30 de abril"
+          amount="R$ 1.450,60"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          lastTransaction="Última transação em 30 de abril"
+          amount="R$ 16.450,60"
+        />
+      </DashboardHighlightCards>
+
+      <DashboardTransactions>
+        <DashboardTransactionsTitle>Listagem</DashboardTransactionsTitle>
+
+        <DashboardTransactionsList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </DashboardTransactions>
     </DashboardContainer>
   );
 }
